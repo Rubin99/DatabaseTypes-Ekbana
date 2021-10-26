c1cb1bfdd97846c2c9e84e33fd19fcba06f9f0c2
Rubin Shrestha <rubinshrestha14@gmail.com>
2021-10-26 12:16:46 +0545

    IMplemented SharedPreferences in Login

--- app/src/main/java/com/example/databasetypes/MainActivity.kt
+++ app/src/main/java/com/example/databasetypes/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.databasetypes
 
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
@@ -9,13 +10,14 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
+
 class MainActivity : AppCompatActivity() {
 
-    private val sharedPrefFile = "testSharedPreference"
+    var sharedPref: SharedPreferences? = null
+    var sharedPrefEditor: SharedPreferences.Editor? = null
 
-
-    private lateinit var nameEditText : EditText
-    private lateinit var passwordEditText : EditText
+    private lateinit var nameEditText: EditText
+    private lateinit var passwordEditText: EditText
 
     private lateinit var viewNameTextView: TextView
     private lateinit var viewPasswordTextView: TextView
@@ -44,48 +46,81 @@ class MainActivity : AppCompatActivity() {
         loginBtn = findViewById(R.id.loginBtn)
         infoTextView = findViewById(R.id.infoTextView)
 
-        val sharedPreferences: SharedPreferences = this.getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE)
+        //val sharedPreferences: SharedPreferences = this.getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE)
+        sharedPref = getSharedPreferences("SharedPrefFile", Context.MODE_PRIVATE);
         checkLogin()
 
         saveBtn.setOnClickListener(View.OnClickListener {
-            val name : String = nameEditText.text.toString()
-            val password : String = passwordEditText.text.toString()
-            val editor : SharedPreferences.Editor = sharedPreferences.edit()
+            val name: String = nameEditText.text.toString()
+            val password: String = passwordEditText.text.toString()
+            sharedPrefEditor = sharedPref?.edit()
 
-            editor.putString("name_key", name)
-            editor.putString("password_key", password)
-            editor.apply()
-            editor.commit()
+
+            sharedPrefEditor?.putString("name_key", name)
+            sharedPrefEditor?.putString("password_key", password)
+            sharedPrefEditor?.apply()
+            sharedPrefEditor?.commit()
         })
         viewBtn.setOnClickListener(View.OnClickListener {
-            val sharedNameValue = sharedPreferences.getString("name_key", "NAME")
-            val sharedPasswordValue = sharedPreferences.getString("password_key", "PASSWORD")
+            val sharedNameValue = sharedPref?.getString("name_key", "NAME")
+            val sharedPasswordValue = sharedPref?.getString("password_key", "PASSWORD")
 
-            if (sharedNameValue.equals("NAME") && sharedPasswordValue.equals("PASSWORD")){
+            if (sharedNameValue.equals("NAME") && sharedPasswordValue.equals("PASSWORD")) {
                 viewNameTextView.setText("Default Name: $sharedNameValue").toString()
                 viewPasswordTextView.setText("Default Password: $sharedPasswordValue").toString()
-            } else{
+            } else {
                 viewNameTextView.setText(sharedNameValue).toString()
                 viewPasswordTextView.setText(sharedPasswordValue).toString()
             }
         })
         clearBtn.setOnClickListener(View.OnClickListener {
-            val editor = sharedPreferences.edit()
-            editor.clear()
-            editor.apply()
+            sharedPrefEditor = sharedPref?.edit()
+            sharedPrefEditor?.clear()
+            sharedPrefEditor?.apply()
             viewNameTextView.setText("").toString()
             viewPasswordTextView.setText("").toString()
         })
 
-        /*loginBtn.setOnClickListener(View.OnClickListener {
-            if (!(nameEditText.text.toString() != "" && passwordEditText.text.toString() != "")){
+        loginBtn.setOnClickListener(View.OnClickListener {
+            if (!(nameEditText.text.toString() != "" && passwordEditText.text.toString() != "")) {
                 infoTextView.text = "Enter valid credentials"
+            } else {
+                doLogin(nameEditText.text.toString(), passwordEditText.text.toString())
             }
-            else{
-              doLogin()
-            }
-        })*/
+        })
 
+    }
+
+    private fun doLogin(userName: String?, userPassword: String?) {
+        try {
+            if (userPassword.equals("123456")) {
+                if (sharedPref == null)
+                    sharedPref = getSharedPreferences("SharedPrefFile", Context.MODE_PRIVATE)
+                sharedPrefEditor = sharedPref?.edit()
+                sharedPrefEditor?.putString("name_key", userName)
+                sharedPrefEditor?.commit()
+
+                val i = Intent(this, SecondActivity::class.java)
+                startActivity(i)
+                finish()
+
+            } else infoTextView.text = "Invalid Credentials"
+        } catch (ex: Exception) {
+            infoTextView.text = ex.message.toString()
+        }
+    }
+
+    private fun checkLogin() {
+        if (sharedPref == null)
+            sharedPref = getSharedPreferences("SharedPrefFile", MODE_PRIVATE)
+
+        val userName = sharedPref?.getString("name_key", "")
+
+        if (userName != null && userName != "") {
+            val i = Intent(this, SecondActivity::class.java)
+            startActivity(i)
+            finish()
+        }
     }
 
 }
--- app/src/main/java/com/example/databasetypes/SecondActivity.kt
+++ app/src/main/java/com/example/databasetypes/SecondActivity.kt
@@ -1,11 +1,68 @@
 package com.example.databasetypes
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class SecondActivity : AppCompatActivity() {
+
+    private lateinit var displayNameTextView: TextView
+    private lateinit var logoutBtn: Button
+
+    var sharedPref: SharedPreferences? = null
+    var sharedPrefEditor: SharedPreferences.Editor? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        displayNameTextView = findViewById(R.id.displayNameTextView)
+        logoutBtn = findViewById(R.id.logoutBtn)
+
+        sharedPref = getSharedPreferences("SharedPrefFile", MODE_PRIVATE);
+        checkLogin();
+
+        logoutBtn.setOnClickListener(View.OnClickListener {
+            logout()
+        })
+    }
+
+    private fun logout() {
+        try {
+            if (sharedPref == null)
+                sharedPref = getSharedPreferences("SharedPrefFile", MODE_PRIVATE)
+            sharedPrefEditor = sharedPref?.edit()
+            sharedPrefEditor?.putString("name_key", "")
+            sharedPrefEditor?.commit()
+
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            finish()
+
+        } catch (ex: Exception) {
+            Toast.makeText(this, ex.message.toString(), Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun checkLogin() {
+        if (sharedPref == null)
+            sharedPref = getSharedPreferences("SharedPrefFile", MODE_PRIVATE)
+
+
+        val userName = sharedPref?.getString("name_key", "")
+
+        if (userName != null && userName != "") {
+            displayNameTextView.text = "Welcome $userName"
+        } else {
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            finish()
+        }
     }
 }
